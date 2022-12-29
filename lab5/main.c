dd899c97b312603bef7f90db92bb4699c3196627
Chiro Liang <Chiro2001@163.com>
2022-12-29 22:43:24 +0800

    buffered queue

--- lab5/main.c
+++ lab5/main.c
@@ -18,15 +18,101 @@ void freeBlock(char *blk) {
   freeBlockInBuffer((unsigned char *) blk, &buf);
 }
 
-// class buffered_queue {
-//   Buffer *buffer;
-//   int sz;
-//   unsigned int addr;
-//
-// public:
-//   buffered_queue(Buffer *buffer, int sz, unsigned int addr) : buffer(buffer), sz(sz), addr(addr) {}
-//
-// };
+char *itoa(unsigned int i) {
+  static char itoa_buffer[5];
+  sprintf(itoa_buffer, "%d", i);
+  return itoa_buffer;
+}
+
+struct buffered_queue_blk {
+  char *blk;
+  unsigned int addr;
+  struct buffered_queue_blk *next;
+  struct buffered_queue_blk *prev;
+};
+typedef struct buffered_queue_blk buffered_queue_blk;
+
+typedef struct {
+  Buffer *buffer;
+  int total;
+  unsigned int addr;
+  buffered_queue_blk *linked_blk;
+  int size;
+  size_t offset;
+} buffered_queue;
+
+buffered_queue *buffered_queue_init(int sz, unsigned addr) {
+  buffered_queue *q = malloc(sizeof(buffered_queue));
+  memset(q, 0, sizeof(buffered_queue));
+  q->buffer = &buf;
+  q->addr = addr;
+  q->total = sz;
+  return q;
+}
+
+void buffered_queue_push(buffered_queue *self, char *tuple) {
+  Log("queue push (%s, %s)", tuple, tuple + 4);
+  if (self->linked_blk == NULL) {
+    Log("init linked blocks, sizeof(buffered_queue_blk) = %lu", sizeof(buffered_queue_blk));
+    self->linked_blk = malloc(sizeof(buffered_queue_blk));
+    self->linked_blk->blk = (char *) getNewBlockInBuffer(self->buffer);
+    self->linked_blk->next = NULL;
+    self->linked_blk->prev = NULL;
+    self->linked_blk->addr = self->addr;
+  }
+  if (self->offset == 56) {
+    // fill in next addr in this block
+    strcpy(self->linked_blk->blk + self->offset, itoa(++self->addr));
+    self->offset = 0;
+    if (self->size == self->total - 1) {
+      buffered_queue_blk *tail = self->linked_blk;
+      while (tail->next != NULL) {
+        tail = tail->next;
+      }
+      buffered_queue_blk *prev = tail->prev;
+      if (prev != NULL) {
+        prev->next = NULL;
+      }
+      Log("queue full, write block %d", tail->addr);
+      writeBlockToDisk((unsigned char *) tail->blk, tail->addr, self->buffer);
+      free(tail);
+    } else {
+      self->size++;
+    }
+    // allocate one buffer, insert to link head
+    Log("new buffer for block %d", self->addr);
+    char *blk = (char *) getNewBlockInBuffer(self->buffer);
+    memset(blk, 0, 64);
+    buffered_queue_blk *n = malloc(sizeof(buffered_queue_blk));
+    Log("n=%p, self->linked_blk=%p", n, self->linked_blk);
+    Assert(n != self->linked_blk, "n == self->linked_blk == %p", n);
+    n->addr = self->addr;
+    n->blk = blk;
+    n->next = self->linked_blk;
+    n->prev = NULL;
+    self->linked_blk->prev = n;
+    self->linked_blk = n;
+  }
+  memcpy(self->linked_blk->blk + self->offset, tuple, 8);
+  self->offset += 8;
+}
+
+void buffered_queue_flush(buffered_queue *self) {
+  Log("buffered_queue_flush");
+  buffered_queue_blk *tail = self->linked_blk;
+  while (tail->next != NULL) {
+    tail = tail->next;
+  }
+  while (tail != NULL) {
+    writeBlockToDisk((unsigned char *) tail->blk, tail->addr, self->buffer);
+    buffered_queue_blk *r = tail;
+    tail = tail->prev;
+    free(r);
+  }
+  self->size = 0;
+  self->linked_blk = NULL;
+  self->offset = 0;
+}
 
 int main() {
   Log("Lab5 program launched!");
@@ -41,6 +127,7 @@ int main() {
   Log("select S.C,S.D from S where S.C = 128");
   Log("======================");
 
+  buffered_queue *q = buffered_queue_init(1, 100);
   unsigned int block = 17;
   while (block != 0) {
     char *blk = NULL;
@@ -50,13 +137,17 @@ int main() {
       int C = -1, D = -1;
       C = atoi(blk + i * 8);
       D = atoi(blk + i * 8 + 4);
-      if (C == 128)
+      if (C == 128) {
         Log("(%s, %s)", blk + i * 8, blk + i * 8 + 4);
+        buffered_queue_push(q, blk + i * 8);
+      }
     }
     block = atoi(blk + 56);
     freeBlock(blk);
     if (block == 49) break;
   }
+  buffered_queue_flush(q);
+  free(q);
 
   freeBuffer(&buf);
 
